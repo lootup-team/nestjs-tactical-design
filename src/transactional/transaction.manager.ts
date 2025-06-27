84bf81b0edb4ad3156de6577004b2fe825f201da
DestyK <nikita.karpov.1910@mail.ru>
2025-06-28 03:15:26 +0800

    fix: `@gedai/nestjs-core` -> `@lootupteam/nestjs-core`

--- src/transactional/transaction.manager.ts
+++ src/transactional/transaction.manager.ts
@@ -1,4 +1,4 @@
-import { ContextService } from '@gedai/nestjs-core';
+import { ContextService } from '@lootupteam/nestjs-core';
 import { Inject, Injectable } from '@nestjs/common';
 import { TransactionStorageKeyToken } from './transaction-manager.token';
 
